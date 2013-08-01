8c111ba7957a4b1f8d12cbfe27c3a559c6066eaa
Mason <mason.delro@gmail.com>
2013-07-31 21:42:17 -0400

    Bind to port 80/downgrade

--- app.js
+++ app.js
@@ -84,6 +84,19 @@ var app = express();
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/sumsurv');
 
+app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+  console.log("Ready");
+
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0) {
+    require('fs').stat(__filename, function(err, stats) {
+      if (err) return console.log(err);
+      process.setuid(stats.uid);
+    })
+  }
+
+});
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
